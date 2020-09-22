b53932b6650d1b0c31734a1047e99d03382efad1
Sheil Sarda <sheilsarda@gmail.com>
2020-09-22 15:02:38 -0400

    Added files

--- Lab1/calculateFK.py
+++ Lab1/calculateFK.py
@@ -112,7 +112,7 @@ class Main():
         A_5_e[1,0]=1
         A_5_e[0,1]=-1
 
-        # T0e = (A_0_1 * A_1_2 * A_2_3 * A_3_4 * A_4_5 * A_5_e)
+    # T0e = (A_0_1 * A_1_2 * A_2_3 * A_3_4 * A_4_5 * A_5_e)
 	# T0e = np.linalg.multi_dot([A_0_1, A_1_2, A_2_3, A_3_4, A_4_5, A_5_e])
 	# T0e = np.linalg.multi_dot([A_5_e, A_4_5, A_3_4, A_2_3, A_1_2, A_0_1])
 	T0e = np.matmul(np.matmul(np.matmul(np.matmul(np.matmul(A_0_1, A_1_2), A_2_3), A_3_4), A_4_5), A_5_e)
